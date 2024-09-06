4cd45c2c04f9fc604a063154cc4ee5099e4b1702
Dario <otrantodaro@gmail.com>
2024-09-05 21:27:23 -0300

    Fix Boid Separation

--- Assets/Scripts/Flocking/Boid.cs
+++ Assets/Scripts/Flocking/Boid.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Flocking
@@ -8,6 +9,10 @@ namespace Flocking
         public float speed = 2.5f;
         public float turnSpeed = 5f;
         public float detectionRadious = 3.0f;
+        public float AlignmentRadious = 1f;
+        public float CohesionRadious = 1f;
+        public float separationRadious = 2f;
+        public float DirectionRadious = 2f;
 
         private Func<Boid, Vector3> Alignment;
         private Func<Boid, Vector3> Cohesion;
@@ -17,23 +22,29 @@ namespace Flocking
         public void Init(Func<Boid, Vector3> Alignment, 
             Func<Boid, Vector3> Cohesion, 
             Func<Boid, Vector3> Separation, 
-            Func<Boid, Vector3> Direction) 
+            Func<Boid, Vector3> Direction,
+            List<float> multipliers) 
         {
             this.Alignment = Alignment;
             this.Cohesion = Cohesion;
             this.Separation = Separation;
             this.Direction = Direction;
+            AlignmentRadious = multipliers[0];
+            CohesionRadious = multipliers[1];
+            separationRadious = multipliers[2];
+            DirectionRadious = multipliers[3];
         }
 
         private void Update()
         {
-            transform.forward = Vector3.Lerp(transform.forward, ACS(), turnSpeed * Time.deltaTime);
-            transform.position += transform.forward * speed * Time.deltaTime;
+            Vector3 acs = ACS();
+            transform.forward = Vector3.Lerp(transform.forward, acs, turnSpeed * Time.deltaTime);
+            transform.position +=  transform.forward * speed * Time.deltaTime;
         }
 
         public Vector3 ACS()
         {
-            Vector3 ACS = Alignment(this) + Cohesion(this) + Separation(this) + Direction(this);
+            Vector3 ACS = (Alignment(this) * AlignmentRadious) + (Cohesion(this) * CohesionRadious) + (Separation(this) * separationRadious) + (Direction(this) * DirectionRadious);
             return ACS.normalized;
         }
     }
--- Assets/Scripts/Flocking/FlockingManager.cs
+++ Assets/Scripts/Flocking/FlockingManager.cs
@@ -11,14 +11,24 @@ namespace Flocking
         public int boidCount = 50;
         public Boid boidPrefab;
         private List<Boid> boids = new List<Boid>();
+        public float AlignmentRadious;
+        public float CohesionRadious;
+        public float separationRadious;
+        public float DirectionRadious;
 
         private void Start()
         {
+            List<float> multipliers = new List<float>();
+            multipliers.Add(AlignmentRadious);
+            multipliers.Add(CohesionRadious);
+            multipliers.Add(separationRadious);
+            multipliers.Add(DirectionRadious);
             for (int i = 0; i < boidCount; i++)
             {
-                GameObject boidGO = Instantiate(boidPrefab.gameObject, new Vector3(Random.Range(-10, 10), Random.Range(-10, 10)), Quaternion.identity);
+                GameObject boidGO = Instantiate(boidPrefab.gameObject,
+                    new Vector3(Random.Range(-10, 10), Random.Range(-10, 10)), Quaternion.identity);
                 Boid boid = boidGO.GetComponent<Boid>();
-                boid.Init(Alignment, Cohesion, Separation, Direction);
+                boid.Init(Alignment, Cohesion, Separation, Direction,multipliers);
                 boids.Add(boid);
             }
         }
@@ -26,24 +36,39 @@ namespace Flocking
         public Vector3 Alignment(Boid boid)
         {
             List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+            
+            if (insideRadiusBoids.Count == 0)   
+                return transform.forward;            
+            
             Vector3 avg = Vector3.zero;
             foreach (Boid b in insideRadiusBoids)
             {
-                avg += b.transform.forward.normalized;
+                if (b == boid) 
+                    continue;  
+                avg += b.transform.forward;
             }
+
             avg /= insideRadiusBoids.Count;
-            avg.Normalize();
-            return avg;
+            return avg.normalized;
         }
 
         public Vector3 Cohesion(Boid boid)
         {
             List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+            
+            if (insideRadiusBoids.Count == 0)   
+                return Vector3.zero;            
+            
+            
             Vector3 avg = Vector3.zero;
             foreach (Boid b in insideRadiusBoids)
             {
+                if (b == boid) 
+                    continue;  
+                
                 avg += b.transform.position;
             }
+
             avg /= insideRadiusBoids.Count;
             return (avg - boid.transform.position).normalized;
         }
@@ -51,14 +76,21 @@ namespace Flocking
         public Vector3 Separation(Boid boid)
         {
             List<Boid> insideRadiusBoids = GetBoidsInsideRadius(boid);
+            
+            if (insideRadiusBoids.Count == 0)
+                return Vector3.zero;
+            
             Vector3 avg = Vector3.zero;
             foreach (Boid b in insideRadiusBoids)
             {
-                avg += (b.transform.position - boid.transform.position).normalized;
+                if (b == boid)
+                    continue;
+                
+                avg += (boid.transform.position - b.transform.position);
             }
+
             avg /= insideRadiusBoids.Count;
-            avg.Normalize();
-            return avg;
+            return avg.normalized;
         }
 
         public Vector3 Direction(Boid boid)
@@ -72,6 +104,9 @@ namespace Flocking
 
             foreach (Boid b in boids)
             {
+                if (b == boid)
+                    continue;
+
                 if (Vector3.Distance(boid.transform.position, b.transform.position) < boid.detectionRadious)
                 {
                     insideRadiusBoids.Add(b);
@@ -85,16 +120,15 @@ namespace Flocking
         {
             if (!Application.isPlaying)
                 return;
-            
-            
+
+
             foreach (Boid boid in boids)
             {
                 Gizmos.color = Color.black;
-                Gizmos.DrawRay(boid.transform.position,Vector3.forward);
-                Gizmos.color = Color.red;
-                Gizmos.DrawWireSphere(boid.transform.position,boid.detectionRadious);
+                Gizmos.DrawRay(boid.transform.position, boid.transform.forward);
+                //Gizmos.color = Color.red;
+                //Gizmos.DrawWireSphere(boid.transform.position,boid.detectionRadious);
             }
-            
         }
     }
 }
